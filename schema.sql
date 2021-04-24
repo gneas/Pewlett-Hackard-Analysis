99c1a1117b1e9f46cd0c51c5fc7400f511a6c7c0
Gabriel <78886203+gneas@users.noreply.github.com>
2021-04-24 14:46:33 -0500

    Adding retirement_info.csv & queries.sql

--- schema.sql
+++ schema.sql
@@ -51,7 +51,12 @@ CREATE TABLE titles (
   	from_date DATE NOT NULL,
   	to_date DATE NOT NULL,
   	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  	PRIMARY KEY (emp_no)
+  	PRIMARY KEY (emp_no, title, from_date)
 );
 
 SELECT * FROM departments;
+SELECT * FROM employees;
+SELECT * FROM dept_manager;
+SELECT * FROM dept_emp;
+SELECT * FROM salaries;
+SELECT * FROM titles;
