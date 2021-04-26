0320470654ac750bcabd623b3b17283d26bb5d34
Gabriel <78886203+gneas@users.noreply.github.com>
2021-04-25 21:52:43 -0500

    Updating challenge queries.

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,5 +1,5 @@
 SELECT e.emp_no, e.first_name, e.last_name, ti.title, ti.from_date, ti.to_date
--- INTO retirement_titles
+INTO retirement_titles
 FROM employees as e
 INNER JOIN titles as ti
 ON e.emp_no = ti.emp_no
